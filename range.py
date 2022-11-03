f4b50f0a016b1727f2383b8271cfc4b4fea19c9b
WojtekLesiczka <116755171+WojtekLesiczka@users.noreply.github.com>
2022-11-03 18:10:25 +0100

    Add files via upload

---
+++ range.py
@@ -0,0 +1,20 @@
+for x in range(1,101):
+    print(x, end="  ")
+
+print()
+print()
+
+for a in range(101,0,-1):
+    print(a, end="  ")
+
+print()
+print()
+
+for b in range(7,82,7):
+    print(b, end="  ")
+
+print()
+print()
+
+for c in range(20,-2,-2):
+    print(c, end="  ")
